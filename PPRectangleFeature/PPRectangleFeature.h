84d4edd0518ed45e282aba140c41e3ed7f00e2d9
Pai Peng <p.peng@s2icode.com>
2017-02-23 10:56:23 +0800

    Initial Commit

---
+++ PPRectangleFeature/PPRectangleFeature.h
@@ -0,0 +1,13 @@
+//
+//  PPRectangleFeature.h
+//  PPRectangleFeature
+//
+//  Created by Pai Peng on 23/02/2017.
+//  Copyright © 2017 Pai Peng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PPRectangleFeature : NSObject
+
+@end
---
+++ PPRectangleFeature/PPRectangleFeature.m
@@ -0,0 +1,13 @@
+//
+//  PPRectangleFeature.m
+//  PPRectangleFeature
+//
+//  Created by Pai Peng on 23/02/2017.
+//  Copyright © 2017 Pai Peng. All rights reserved.
+//
+
+#import "PPRectangleFeature.h"
+
+@implementation PPRectangleFeature
+
+@end
